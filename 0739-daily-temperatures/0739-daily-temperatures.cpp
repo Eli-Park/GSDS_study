aaf5af307792fc6015f69512a2d05db2f3722b6a
Eli-Park <46877586+Eli-Park@users.noreply.github.com>
2023-07-29 17:36:07 +0900

    Time: 156 ms (99.78%), Space: 107.4 MB (18.91%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-/*
-The idea here is to maintain a stack of decreasing order. If the incoming element is greater than the top of the stack, we pop from the stack and add the new element.
-
-*/
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-      vector<int> ans(temperatures.size(), 0);
-      stack<pair<int, int>> s;
-      
-      for (int i = 0; i < temperatures.size(); i++){
-          auto temp = temperatures[i];
-          while(!s.empty() && temp > s.top().first){
-              auto toPop = s.top();
-              s.pop();
-              ans[toPop.second] = i - toPop.second;
-          }
-          if(s.empty() || temp <= s.top().first){
-              s.push({temp, i});
-          }
-      }
-      return ans;
+        vector<int> output(temperatures.size(), 0);
+        stack<pair<int,int>> s;
+
+        for (int i = 0; i < temperatures.size(); i++) {
+            int cur = temperatures[i];
+
+            while(!s.empty() && cur > s.top().first) {
+                auto compar = s.top();
+                s.pop();
+                output[compar.second] = i - compar.second;
+            }
+
+            if(s.empty() || cur <= s.top().first) {
+                s.push({cur, i});
+            }
+
+
+        }
+        return output;
+
+        
     }
 };
